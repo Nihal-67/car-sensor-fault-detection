7d635f32c32e5d15534dff273d576a3620bd565d
NIHAL SINGH <nihal.r.singh.67@gmail.com>
2022-11-02 10:40:56 +0530

    created basic project structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+
+    name="sensor",
+    version="0.0.1",
+    author="Nihal Singh",
+    author_email="nihal.r.singh.67@gmail.com",
+    packages= find_packages()
+)
