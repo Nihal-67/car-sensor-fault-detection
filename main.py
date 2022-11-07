a6e237bbd4680890460dc1c157310c8063e3720e
NIHAL SINGH <nihal.r.singh.67@gmail.com>
2022-11-07 19:04:02 +0530

    created data validation flow

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ from sensor.logger import logging
 import os
 from sensor.entity.config_entity import TrainingPipelineConfig, DataIngestionConfig
 from sensor.pipeline.training_pipeline import TrainPipeline
+from sensor.configuration.mongodb_connection import MongoDBClient
 
 if __name__ == '__main__':
-    training_pipeline= TrainPipeline()
-    training_pipeline.run_pipeline()
+    database = MongoDBClient()
+    print (database.database_name)
